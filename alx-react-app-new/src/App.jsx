4dedda6365f1871bdcf498002888b9e734582f3c
Atokolo John <atokoloj@gmail.com>
2025-07-16 23:17:31 +0100

    added file

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
-import WelcomeMessage from '../src/components/WelcomeMessage'
-import Header from '../src/components/Header'
-import MainContent from '../src/components/MainContent'
-import Footer from '../src/components/Footer' 
+import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer' 
 import UserProfile  from './components/UserProfile' 
+import Counter from './components/Counter'
 
 function App() {
   return (
@@ -12,7 +13,9 @@ function App() {
       <Header />
       <MainContent />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <Counter />
       <Footer />
+      
     </>
   )
 }
