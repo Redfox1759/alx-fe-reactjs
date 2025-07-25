577b61f18fd1cffe0bea15f59e6872fb79eee9ef
Atokolo John <atokoloj@gmail.com>
2025-07-25 15:08:36 +0100

    Updated file

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,16 +1,27 @@
-import React from 'react'
-import RecipeList from './component/RecipeList' 
-import AddRecipeForm from './component/AddRecipeForm' 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import RecipeList from './components/RecipeList';
+import AddRecipeForm from './components/AddRecipeForm';
+import RecipeDetails from './components/RecipeDetails';
+import SearchBar from './components/SearchBar';
+import FavoritesList from './components/FavoritesList';
+import RecommendationsList from './components/RecommendationsList'
 
-const App = () => {
+function App() {
   return (
-    <div>
-      
-      <RecipeList />
-      <AddRecipeForm />
-      
-    </div>
-  )
+    <Router>
+      <div>
+        <h1>Recipe Sharing App</h1>
+        <SearchBar />
+        <AddRecipeForm />
+        <FavoritesList />
+        <RecommendationsList />
+        <Routes>
+          <Route path="/" element={<RecipeList />} />
+          <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,20 +1,23 @@
-import { useRecipeStore } from './recipeStore';
+import { Link } from 'react-router-dom';
+import useRecipeStore from '../components/recipeStore';
 
 const RecipeList = () => {
-
-const recipes = useRecipeStore(state => state.recipes);
-
-    return (
-        <div>
-        {recipes.map(recipe => (
-            <div key={recipe.id}>
-            <h3>{recipe.title}</h3>
-            <p>{recipe.description}</p>
-            </div>
-        ))}
+  const filteredRecipes = useRecipeStore(state => state.filteredRecipes.length ? state.filteredRecipes : state.recipes);
+  const addFavorite = useRecipeStore(state => state.addFavorite);
+  
+  return (
+    <div>
+      <h2>Recipe List</h2>
+      {filteredRecipes.map(recipe => (
+        <div key={recipe.id}>
+          <h3>{recipe.title}</h3>
+          <p>{recipe.description}</p>
+          <button onClick={() => addFavorite(recipe.id)}>Favorite</button>
+          <Link to={`/recipe/${recipe.id}`}>View Details</Link>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default RecipeList;
-
