83588ba50203cb3ad453bc4271e763a4f6d2bfa5
Atokolo John <atokoloj@gmail.com>
2025-07-25 14:30:11 +0100

    updated file

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import RecipeList from './component/RecipeList' 
+import AddRecipeForm from './component/AddRecipeForm' 
+
+const App = () => {
+  return (
+    <div>
+      
+      <RecipeList />
+      <AddRecipeForm />
+      
+    </div>
+  )
+}
+
+export default App
